cce8d95031e0290e30793489cec4fb57170210b2
sahithiharness <sahithi.banda@harness.io>
2024-11-30 19:40:46 +0530

    fix-1

--- autodetect/inject_gradle.go
+++ autodetect/inject_gradle.go
@@ -38,7 +38,7 @@ initscript {
                 url System.getenv('MAVEN_URL')
             }
         } else {
-         
+			mavenCentral()       
     }
     dependencies {
         classpath 'io.harness:gradle-cache:%s'
